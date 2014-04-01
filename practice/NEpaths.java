901f4ea90852233c4e1dee345557d814bad7e42f
Kaung Htet Aung <tommy.aung7@gmail.com>
2014-04-02 00:16:51 +0800

    Practice33

---
+++ practice/NEpaths.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class NEpaths {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter rows apart: ");
+		int rows = sc.nextInt();
+		System.out.print("Enter columns apart: ");
+		int cols = sc.nextInt();
+
+		String path = "";
+		System.out.println("Number of paths = " + ne(rows, cols, path));
+	}
+	// To compute the number of NE paths as well as to display the paths
+	public static int ne(int rows, int cols, String path) {
+		int noOfPaths = 0;
+		if(rows == 0 && cols == 0){
+			System.out.println(path);
+			return 1;
+		}
+		if(rows!=0){
+			noOfPaths += ne(rows-1,cols,path+"N ");
+		}
+		if(cols!=0){
+			noOfPaths += ne(rows,cols-1,path+"E ");
+		}
+		return noOfPaths;
+	}
+}
+
